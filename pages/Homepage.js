067bbd82797757eadaffb31662acf85d0ac492c2
Donato Bitetto <giotto9712@gmail.com>
2024-08-06 18:35:32 +0200

    infinite scrolling

--- pages/Homepage.js
+++ pages/Homepage.js
@@ -6,8 +6,9 @@ import MovieDetail from "./MovieDetail";
 export default async function Homepage() {
 	const url = import.meta.env.VITE_MOVIEDB_URL;
 	const token = import.meta.env.VITE_MOVIEDB_KEY;
+	let page = 1;
 
-	const response = await fetch(`${url}/movie/upcoming`, {
+	const response = await fetch(`${url}/movie/upcoming?page=${page}`, {
 		method: "GET",
 		headers: {
 			Authorization: `Bearer ${token}`,
@@ -16,17 +17,32 @@ export default async function Homepage() {
 	const data = await response.json();
 
 	document.querySelector("#app").innerHTML = `
-                <div class="container mx-auto px-4">
-                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-10 z-50">
-                        ${data.results.map((movie) => MovieCard(movie)).join("")}
-                    </div>
-                </div>
-            `;
+		<div class="container mx-auto px-4">
+			<div id="grid" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-10 z-50">
+				${data.results.map((movie) => MovieCard(movie)).join("")}
+			</div>
+		</div>
+	`;
 
 	/* cards animation */
 	inView(".grid > div", ({ target }) => {
 		let duration = 1.5;
 		animate(target, { opacity: 1 }, { duration });
 		return ({ target }) => animate(target, { opacity: 0 }, { duration });
+	});
+
+	window.addEventListener("scroll", async () => {
+		if(window.innerHeight+window.scrollY > document.querySelector("#app").clientHeight - 400){
+			page++;
+			const response = await fetch(`${url}/movie/upcoming?page=${page}`, {
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			const data = await response.json();
+			console.log(data);
+			document.querySelector("#grid").innerHTML += data.results.map((movie) => MovieCard(movie)).join("")
+		}
 	});
 }
